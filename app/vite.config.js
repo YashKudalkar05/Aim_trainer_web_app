13511defb2363c892bf896fd877a2b36e6193ed8
YashKudalkar05 <81027078+YashKudalkar05@users.noreply.github.com>
2026-04-22 22:39:13 -0400

    vite base update 2

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Aim-trainer-web-app/',
+  base: '/Aim_trainer_web_app/',
 })
